11fda6744b53786ed5cdee651d29c65b69e5ce10
miguelAngel <miguelAngel@gmail.com>
2021-03-12 14:01:51 +0100

    CONSTRUCTOR

--- calcularVersiones/src/calcularVersiones/CalculadoraVer.java
+++ calcularVersiones/src/calcularVersiones/CalculadoraVer.java
@@ -7,10 +7,13 @@ public class CalculadoraVer {
 		this.numero1=num1;
 		this.numero2=num2;
 	}
-	
+	public void suma(int num1,int num2){
+		int num=num1+num2;
+		System.out.println(num);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
