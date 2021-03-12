a1b9fc2611c414b71dcf2a0c71d97e9e7b2812e3
miguelAngel <miguelAngel@gmail.com>
2021-03-12 14:15:38 +0100

    3

--- calcularVersiones/src/calcularVersiones/CalculadoraVer.java
+++ calcularVersiones/src/calcularVersiones/CalculadoraVer.java
@@ -10,6 +10,8 @@ public class CalculadoraVer {
 	public void suma(int num1,int num2){
 		int RESULTADO=num1+num2;
 		System.out.println(RESULTADO);
+		
+		//HOLA
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
