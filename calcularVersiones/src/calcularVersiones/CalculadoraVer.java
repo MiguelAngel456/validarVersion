5a7da65c77417a7afce611235bee395ee15e8757
miguelAngel <miguelAngel@gmail.com>
2021-03-12 13:48:43 +0100

    constructor

--- calcularVersiones/src/calcularVersiones/CalculadoraVer.java
+++ calcularVersiones/src/calcularVersiones/CalculadoraVer.java
@@ -1,7 +1,13 @@
 package calcularVersiones;
 
 public class CalculadoraVer {
-
+	int numero1,numero2;
+	
+	public CalculadoraVer(int num1, int num2){
+		this.numero1=num1;
+		this.numero2=num2;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
